fa5406a070456e4930c39efe56e42f3df075f7c5
João Vitor <joaovitorxvasconcelos@gmail.com>
2025-06-17 00:26:46 -0300

    upload 1
    remoção de exemplo de uso do objeto colaborador no main

--- pontoJava/Main.java
+++ pontoJava/Main.java
@@ -2,10 +2,6 @@ package pontoJava;
 
 public class Main {
     public static void main(String[] args) {
-        Colaborador alberto = new Colaborador("Alberto", "Assistente");
-        alberto.registrarEntrada();
-        alberto.registrarSaida();
-        alberto.printRegistros();
         
         
     }
